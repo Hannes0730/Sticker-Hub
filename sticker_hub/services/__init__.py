a569464bae0884a5157cc46a2b6b5c19594b7ba2
Edward Tapado <edwardtapado99@gmail.com>
2026-04-19 05:50:39 +0800

    feat: expose update installation functions in module exports

--- sticker_hub/services/__init__.py
+++ sticker_hub/services/__init__.py
@@ -1,6 +1,7 @@
 from .cache_service import StickerCache
 from .downloader import StickerDownloadManager
 from .import_resolver import resolve_sticker_urls, upgrade_sticker_urls_file
+from .update_install_service import UpdateInstallResult, start_in_app_update
 from .update_service import UpdateCheckResult, check_for_update
 
 __all__ = [
@@ -9,6 +10,8 @@ __all__ = [
 	"resolve_sticker_urls",
 	"upgrade_sticker_urls_file",
 	"UpdateCheckResult",
+	"UpdateInstallResult",
 	"check_for_update",
+	"start_in_app_update",
 ]
 
